f3b3e9540918306cedc7964069ed3e2a46631d8c
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2023-05-31 18:38:22 -0600

    Start app

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [openCamera, setOpenCamera] = useState(false);
+
+  const handleOpenCamera = () => {
+    setOpenCamera(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {openCamera ? (
+        <CameraScreen onCloseCamera={() => setOpenCamera(false)} />
+      ) : (
+        <View style={styles.home}>
+          <Button title="Open Camera" onPress={handleOpenCamera} />
+        </View>
+      )}
+    </View>
+  );
+}
+
+function CameraScreen({ onCloseCamera }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      setPhoto(photo.uri);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={(ref) => setCamera(ref)}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Flip"
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          />
+          <Button title="Take Picture" onPress={takePicture} />
+          <Button title="Close Camera" onPress={onCloseCamera} />
+        </View>
+      </Camera>
+      {photo && <Text>{photo}</Text>}
     </View>
   );
 }
@@ -13,8 +74,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+    backgroundColor: 'transparent',
+  },
+  home: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
